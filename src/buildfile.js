e93085f400f3a33388ad573a80972ff5a75187ca
Jackson Kearl <jakearl@microsoft.com>
2019-10-28 15:28:30 -0700

    sdafasd

--- src/buildfile.js
+++ src/buildfile.js
@@ -1,7 +1,6 @@
 /*---------------------------------------------------------------------------------------------
  *  Copyright (c) Microsoft Corporation. All rights reserved.
  *  Licen
- *
- *
+
  * sed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
