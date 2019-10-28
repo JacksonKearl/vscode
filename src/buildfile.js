520c1b6d2004d5997b5022ea741afca2f7670c86
Jackson Kearl <jakearl@microsoft.com>
2019-10-28 15:28:04 -0700

    asdfsdf

--- src/buildfile.js
+++ src/buildfile.js
@@ -2,9 +2,3 @@
  *  Copyright (c) Microsoft Corporation. All rights reserved.
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
-
-exports.base = [{
-	name: 'vs/base/common/worker/simpleWorker',
-	include: ['vs/editor/common/services/editorSimpleWorker'],
-	prepend: ['vs/loader.js']
-}];
